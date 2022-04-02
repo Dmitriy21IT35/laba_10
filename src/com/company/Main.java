c187441e8f5ab1041a435317654d5a139f4910ec
Dima <dimaforevermelnik@gmail.com>
2022-04-02 14:31:32 +0300

    как то так

---
+++ src/com/company/Main.java
@@ -0,0 +1,19 @@
+package com.company;
+
+public class Main {
+        public static void main(String[] args) {
+            String sty = ("The cat went into the forest!\n");
+            boolean truefalse = false;
+            if (truefalse) {
+                System.out.println (true);
+            }
+            boolean n1 = sty.endsWith ("has come\n");
+            boolean n2 = sty.endsWith ("has left\n");
+            if (n1 = true)
+                System.out.println ("true");
+
+            System.out.println ("String " + sty + " has come?\n " + n1);
+            System.out.println ("String " + sty + " has left?\n " + n2);
+        }
+
+    }
